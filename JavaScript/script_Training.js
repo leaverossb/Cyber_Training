5a5ef8e5dc857b47e7818b5b2d72bf2911b0f594
Phillip Ross <leaverossb@gmail.com>
2018-08-01 16:44:38 -0400

    initial commit

---
+++ JavaScript/script_Training.js
@@ -0,0 +1,20 @@
+
+
+//navigation buttons
+
+
+	function Network_Basics() {
+		window.open("Training/Network Basics.html","Home Network","width=1000,height=700");		
+	}	//syntax window.open(URL, name, specs, replace)  specs must be a comma separated list, no white spaces.  numerous values are supported
+
+	function Component_Basics() {
+		window.open("","","width=500,height=300");		
+	}	//syntax window.open(URL, name, specs, replace)  specs must be a comma separated list, no white spaces.  numerous values are supported
+	
+	function Defend_Net() {
+		window.open("","","width=500,height=300");		
+	}	//syntax window.open(URL, name, specs, replace)  specs must be a comma separated list, no white spaces.  numerous values are supported
+	
+	
+//Training Window
+	

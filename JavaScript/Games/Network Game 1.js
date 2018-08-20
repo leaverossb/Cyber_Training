0e0266930374aa11dfea89fb6202d22fe14271f3
Ross <phillip.ross@ky.gov>
2018-08-20 13:08:30 -0400

    changes to training page

---
+++ JavaScript/Games/Network Game 1.js
@@ -0,0 +1,3 @@
+
+		
+alert("i'm reading your javascript file");   //this is a way to test if the html file is reading this file
---
+++ JavaScript/Games/test game.js
@@ -0,0 +1,13 @@
+
+
+
+
+/* VARIABLES */
+
+
+
+/* FUNCTIONS */
+
+
+	
+alert("i'm reading your javascript file");   //this is a way to test if the html file is reading this file
